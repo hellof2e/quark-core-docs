6672e40a215e6062fd193829f64997d56c54882b
zhaocun682 <zhaocun682@hellobike.com>
2023-02-20 16:15:19 +0800

    feat： 多语言配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,21 +2,46 @@ module.exports = {
     title: 'Quark',
     description: 'Just playing around',
     themeConfig: {
-        nav: [
-            { text: 'Guide', link: '/guide/introduc/' },
-            { text: 'Quark Design', link: 'https://quark-design.hellobike.com/' },
-            { text: 'Playground', link: 'https://quark-design.hellobike.com/' },
-        ],
-        sidebar: {
-            '/guide/': [
-                {
-                    title: 'Quark',
-                    children: [
-                        ['introduc', '简介'],
-                        ['install', '安装']
+        locales: {
+            '/': {
+                lang: 'en-US',
+                nav: [
+                    { text: 'Guide', link: '/guide/introduc/' },
+                    { text: 'Quark Design', link: 'https://quark-design.hellobike.com/' },
+                    { text: 'Playground', link: 'https://quark-design.hellobike.com/' },
+                ],
+                sidebar: {
+                    '/guide/': [
+                        {
+                            title: 'Quark',
+                            children: [
+                                ['introduc', 'Introduc'],
+                                ['install', 'Install']
+                            ]
+                        }
                     ]
                 }
-            ]
+            },
+            '/zh/': {
+                lang: 'zh-CN',
+                nav: [
+                    { text: '指南', link: '/guide/introduc/' },
+                    { text: 'Quark Design', link: 'https://quark-design.hellobike.com/' },
+                    { text: 'Playground', link: 'https://quark-design.hellobike.com/' },
+                ],
+                sidebar: {
+                    '/guide/': [
+                        {
+                            title: 'Quark',
+                            children: [
+                                ['introduc', '简介'],
+                                ['install', '安装']
+                            ]
+                        }
+                    ]
+                }
+            },    
         }
+        
     }
   }
