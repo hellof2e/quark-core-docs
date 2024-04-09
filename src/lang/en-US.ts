c212d44235ff77d385c39cbe1089761be7258071
xsf0105 <allan91coder@gmail.com>
2024-04-09 16:05:56 +0800

    feat: web site update

--- src/lang/en-US.ts
+++ src/lang/en-US.ts
@@ -1,34 +1,26 @@
 export default {
-  homeTitle: "Quarkc, born for cross technology stack!",
-  homeSubtitle: "Web Components Framework",
+  homeSubtitle: "Web Components Toolkit",
   getStarted: "Get Started",
-  getStartedQuarkc: "Build your component",
-  getStartedQuarkd: "Components Docs",
-  whyQuark: "Why Quark?",
-  scrollDown: "Scroll Down",
-  weBelieve: "WE BELIEVE",
-  weBelieveDesc1:
-    "The component library can exist without relying on the technical framework.",
-  weBelieveDesc2:
-    "If this can be done, then the component library can go through ups and downs and go through a long period of time!",
-  weBelieveDesc3:
-    "It does not need to be updated with the update of the technology stack.",
-  advantage: "Advantages",
-  box1Title: "Small size",
+
+  box1Title: "Build page",
   box1Desc:
-    "The built components do not need to rely on third-party runtimes, and the average volume of each page or component is about 1kb ~ 2kb.",
-  box2Title: "Browser native",
+    "A complete template is provided, no configuration is required, and it can be used right out of the box! Routing, global state management, vite packaging.",
+
+  box2Title: "Build components",
   box2Desc:
-    "Relying on Web components technology, components can be used by any front-end framework.",
-  box3Title: "Future oriented",
+    "Relying on Web components technology, you can easily build components across technology stacks.",
+
+  box3Title: "face to the future",
   box3Desc:
-    "Unlike other third-party component building methods, Quark do not rely on front-end frameworks, and do not need to be updated with technology stack updates.",
-  whatDesc1:
-    "Quarkc (Quark core): A Web Components compiler. It allows you to implement a custom cross-stack/frame-free native component with minimal cost and efficiency.",
-  whatDesc2:
-    "The data modification of the component is handled by the browser's own API, which is simpler and more efficient!",
-  whatDesc3:
-    "Component data modification is handled by the browser's own API, which is simpler and more efficient!",
-  readPage: "Read this article",
-  knowMore: "to learn more.",
+    "Unlike other page/component construction methods, Quark does not rely on front-end frameworks and does not need to be updated as the technology stack is updated.",
+
+  box4Title: "Extremely small",
+  box4Desc:
+    "Components built with Quarkc do not need to rely on third-party runtime, and the average size of each page/component is about 1kb ~ 2kb.",
+
+  box5Title: "Browser native",
+  box5Desc: "The build artifacts can be used by any front-end framework.",
+
+  box6Title: "JSX syntax",
+  box6Desc: "Quarkc supports using JSX syntax, easy to get started",
 };
--- src/lang/zh-CN.ts
+++ src/lang/zh-CN.ts
@@ -1,16 +1,6 @@
 export default {
-  homeTitle: "Quarkc，为跨技术栈而生！",
   homeSubtitle: "Web Components 框架",
   getStarted: "Get Started",
-  getStartedQuarkc: "构建你的 Web 组件",
-  getStartedQuarkd: "组件库文档",
-  whyQuark: "为什么选择 Quark？",
-  scrollDown: "Scroll Down",
-  weBelieve: "我们认为",
-  weBelieveDesc1: "组件可以不用依赖技术框架而存在。",
-  weBelieveDesc2: "如果能做到这点，那么这个组件可以穿越沉浮，历经长久!",
-  weBelieveDesc3: "不用随着技术栈的更新而更新。",
-  advantage: "有什么优势",
 
   box1Title: "构建页面",
   box1Desc:
@@ -28,8 +18,8 @@ export default {
     "用 Quarkc 构建的组件无需依赖第三方 runtime，平均每个页面/组件体积 1kb ~ 2kb 左右。",
 
   box5Title: "浏览器原生",
-  box5Desc: "依托于 Web components 技术，构建产物可以被任何前端框架使用。",
+  box5Desc: "构建产物可以被任何前端框架使用。",
 
   box6Title: "JSX 语法",
-  box6Desc: "Quarkc 支持使用 JSX 语法，无需学习任何新语法。",
+  box6Desc: "Quarkc 支持使用 JSX 语法，轻松上手。",
 };
